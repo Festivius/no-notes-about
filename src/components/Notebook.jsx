42d1a5e1e42421af7ecea7ec4ff098a59c05d869
Diwen Huang <diwenhuang@Mac.ht.home>
2025-03-15 16:45:43 -0700

    hello

--- src/components/Notebook.jsx
+++ src/components/Notebook.jsx
@@ -1,8 +1,13 @@
 import './Home.css'
 function Notebook() {
     return (
-      <div className="notebook-container">
-        <a href="#" className="nonotes-button" onClick="window.location.href='no-notes.vercel.app'">TRY NONOTESSSS!</a>
+        <div className="notebook-container">
+        <button 
+          className="nonotes-button" 
+          onClick={() => window.location.href = 'https://no-notes.vercel.app'}
+        >
+          TRY NONOTESSSS!
+        </button>
       </div>
     );
   }
