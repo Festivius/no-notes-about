2bf5313e5d819ece7fbb1c7c35b723d8c69fced5
Diwen Huang <diwenhuang@Mac.ht.home>
2025-03-15 17:46:40 -0700

    aobut

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,24 @@ function About() {
         
         <div className="about-text">
           <p>
-            hi, ever wanted to improve your writing? use nonotes. 
+            ever felt like taking notes was too easy?
+            <br></br>
+            - use nonotes.
+            <br></br>
+            ever felt like your grades were too high?
+            <br></br>
+            - use nonotes.
+            <br></br>
+            ever wanted to hate yourself?
+            <br></br>
+            - use nonotes.
+          </p>
+          <div className="about-divider"></div>
+          <p>
+            nonotes is made with react, tailwind css, and javascript.
+            we include a dynamic interface equipped with a horrible ai assistance,
+            terrible writing experience, and a whole lot of headache.
+
           </p>
           
           <div className="about-divider"></div>
@@ -25,7 +42,7 @@ function About() {
           <div className="about-card">
             <h3>our philosophy</h3>
             <p>
-              we don't have one.
+              we don't have one. and you shouldnt either.
             </p>
           </div>
         </div>
